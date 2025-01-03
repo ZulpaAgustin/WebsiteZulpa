7ae6b0c44fd3ad07caeeed7276ff67f0566ffee4
Zuzull <113565616+ZulpaAgustin@users.noreply.github.com>
2025-01-03 10:29:41 +0700

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,6 +3,7 @@
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100"><path fill="#004080" fill-opacity="1" d="M0,128L1440,0L1440,320L0,320Z"></path></svg>
     <div class="footer-text">
       <p>Copyright <i class="fa-regular fa-copyright"></i> SMKN 4 Tasikmalaya 2024 - {{ year }}</p>
+      <div>Made with ❤️ by Zulpa</div>
     </div>
   </div>
 </template>
